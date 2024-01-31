243874edfd98e34059f657e4b664ec19dc9f98b4
Alvaro Kumenius <akumenius@gmail.com>
2024-01-31 10:41:33 +0100

    Canvi de logo

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState } from 'react';
 import Link from 'next/link';
-import styles from './Header.module.css';
 import { Offcanvas, Button } from 'react-bootstrap';
 //import { IconLogin2 } from '@tabler/icons-react';
 
@@ -16,8 +15,8 @@ const Header = () => {
     };
 
     return (
-        <header className="p-4 flex justify-between items-center bg-zinc-300">
-            <div className={styles.logo}></div>
+        <header className="p-3 flex justify-between items-center bg-zinc-300">
+            <img class="rounded" src="/paypart.png" alt="" width="50px" height="50px"/>
             <h1>PayPart</h1>
             <button><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-login-2" width="36" height="36" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" /><path d="M3 12h13l-3 -3" /><path d="M13 15l3 -3" /></svg></button>
             <Button onClick={handleShow}>
@@ -25,7 +24,7 @@ const Header = () => {
             </Button>
             <Offcanvas style={offcanvas} show={show} onHide={handleClose}>
                 <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                    <Offcanvas.Title>Titol</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                     <nav class="nav flex-column">
