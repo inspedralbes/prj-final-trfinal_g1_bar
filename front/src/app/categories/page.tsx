fffb8398203b0843d7afaf8f9ff2921434684c52
Alvaro Kumenius <akumenius@gmail.com>
2024-01-31 10:25:54 +0100

    Navbar

--- front/src/app/categories/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Categories() {
-    return <p>Aquest és el contingut de la pàgina Categories</p>
-}
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Header from '../components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
     <div>
-      <Header />
-      {/* Other components or content */}
+      
     </div>
   );
 };
