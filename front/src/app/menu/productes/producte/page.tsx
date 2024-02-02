24f3b7948963bc0dc7c3a05ae98d448053068977
Alvaro Kumenius <akumenius@gmail.com>
2024-02-02 11:53:06 +0100

    Modificar icones i guardar producte al tiquet

--- front/src/app/menu/productes/producte/page.tsx
+++ front/src/app/menu/productes/producte/page.tsx
@@ -3,12 +3,16 @@
 import React, { useState, useEffect } from 'react';
 import { RootState } from "@/lib/store";
 import { useSelector, useDispatch } from 'react-redux';
+import { setTiquetIndividual } from "@/lib/Features/getRestaurant";
 
 export default function Producte() {
 
-    let productesCategoriaSeleccionada = useSelector((state: RootState) => state.restaurant.restaurant.productesCategoriaVisualitzada);
+    const productesCategoriaSeleccionada = useSelector((state: RootState) => state.restaurant.restaurant.productesCategoriaVisualitzada);
     const producteId = useSelector((state: RootState) => state.restaurant.restaurant.producteId);
+    const tiquetIndividual = useSelector((state: RootState) => state.restaurant.restaurant.tiquetIndividual);
     let producteSeleccionat;
+
+    const dispatch = useDispatch();
 
     for (let i = 0; i < productesCategoriaSeleccionada.length; i++) {
         if (productesCategoriaSeleccionada[i].id === producteId) {
@@ -19,16 +23,26 @@ export default function Producte() {
     // Estat per gestionar la variable quantitat
     const [quantitat, setQuantitat] = useState(1);
 
-    // Function to handle button click and update the count
-    const suma = () => {
+    function suma() {
         setQuantitat(quantitat + 1);
     };
 
-    const resta = () => {
+    function resta() {
         if (quantitat > 1) {
             setQuantitat(quantitat - 1);
         }
     };
+
+    function afegirProducteTiquetIndividual() {
+        
+        const producteTiquet = {
+          tiquet_id: 1,
+          producte_id: producteSeleccionat.id,
+          quantitat: quantitat
+        };
+
+        dispatch(setTiquetIndividual([producteTiquet]));
+      }
 
     return (
         <div className='pt-4 pb-5'>
@@ -41,7 +55,7 @@ export default function Producte() {
                 <div className='mt-3 row'>
                     <div className="botonsSumaResta p-0">
                         <button onClick={resta}>-</button>
-                        <input type="number" value={quantitat} className="inputN"></input>
+                        <input type="number" value={quantitat}></input>
                         <button onClick={suma}>+</button>
                     </div>
                 </div>
@@ -50,7 +64,10 @@ export default function Producte() {
                     <h3 className="p-0">{producteSeleccionat.preu} &euro;</h3>
                     <p className="p-0 font-italic">{producteSeleccionat.descripcio}</p>
                 </div>
-                <div className='row mt-4'>
+                <div className='mt-3 row'>
+                    <button onClick={afegirProducteTiquetIndividual} type="button" className="col-6 btn btn-primary float-right">AFEGIR A LA COMANDA</button>
+                </div>
+                <div className='row mt-3'>
                     <div className='modificar-ingredients px-4 py-2 bg-zinc-300 d-flex justify-content-between align-items-center'>
                         <p className='m-0'>MODIFICAR INGREDIENTS</p>
                         <button className='d-flex justify-content-center align-items-center bg-secondary bg-opacity-75'>+</button>
