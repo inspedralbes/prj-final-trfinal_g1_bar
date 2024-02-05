a9dd34b23845ab35f17bc0e1bbc727f22ca69763
Alvaro Kumenius <akumenius@gmail.com>
2024-02-05 10:06:53 +0100

    Mostrar productes per usuari

--- front/src/app/comanda/comanda-alt/page.tsx
+++ front/src/app/comanda/comanda-alt/page.tsx
@@ -8,6 +8,7 @@ export default function ComandaAlternativa() {
     const [tiquet, setTiquet] = useState([]);
     const [loading, setLoading] = useState(true);
     const url = `http://localhost:8000/api/tiquets/${id}`;
+    const [arrayUsuaris, setArrayUsuaris] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -31,24 +32,51 @@ export default function ComandaAlternativa() {
         fetchData();
     }, []); // <- s'ha d'especificar una array buida com a segon argument de useEffect per a que només s'executi un cop. Si no s'especifica res el hook useEffect s'executarà de manera infinita
 
+    useEffect(() => {
+        // Call formatUsuaris only when tiquet.tiquets is defined
+        if (tiquet.tiquets) {
+            const formattedUsers = formatUsuaris();
+            setArrayUsuaris(formattedUsers);
+        }
+    }, [tiquet]);
+
+
     function formatUsuaris() {
         let arrayUsuaris = [];
+        let producteTiquet;
+
         for (let i = 0; i < tiquet.tiquets.length; i++) {
             producteTiquet = tiquet.tiquets[i];
-            producteTiquet.pivot.user_id;
+            if (!arrayUsuaris.some(usuari_id => usuari_id == producteTiquet.pivot.user_id)) {
+                arrayUsuaris.push(producteTiquet.pivot.user_id)
+            }
         }
+        console.log(arrayUsuaris);
+        return arrayUsuaris;
     }
 
     return (
-        <div className="d-flex flex-column align-items-center">
-       {tiquet.tiquets.map((product, index) => (
-        <div key={index}>
-          <p>ID: {product.id}</p>
-          <p>Name: {product.nom}</p>
-          <p>Description: {product.descripcio}</p>
-          {/* Render other properties as needed */}
-        </div>
-      ))}
+        <div className='pt-4 pb-5 px-6'>
+            <div className="d-flex flex-column">
+                {arrayUsuaris.map((usuari) => (
+                    <div className='mb-5'>
+                        <p className='position-relative'>ID_USUARI: {usuari}</p>
+                        <div className='d-flex flex-row gap-4 items-container'>
+                            {tiquet.tiquets.map((producte, index) => (
+                                // Renderitza la informació del producte sempre i quan usuari sigui igual a product.pivot.user_id
+                                usuari === producte.pivot.user_id && (
+                                    <div key={index} className='item-categoria bg-zinc-300 rounded shadow d-flex flex-column justify-content-end align-items-center'>
+                                        <img className="img-producte" src="/salad.png" alt="" />
+                                        <div className='pb-3 text-center fw-bold text-uppercase'>
+                                            {producte.nom}
+                                        </div>
+                                    </div>
+                                )
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
