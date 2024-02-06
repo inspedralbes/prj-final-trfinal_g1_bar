4520c2aaf6e8e6b97b18f2e9074e5a58680753a4
Dasbits <a20davsalsos@inspedralbes.cat>
2024-02-06 11:39:19 +0100

    login afegit sense problemes

---
+++ front-rest/pages/register.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="login-container" >
+        <h1>Registre</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            password_confirmation: ''
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+<style>
+
+</style>
