f1a6e362a6dd63288edb71a05a0dee5cbbc3c3d9
santipu03 <santipujolsau@gmail.com>
2024-02-07 08:13:02 +0100

    crear store dades usuari

--- front/src/lib/store.ts
+++ front/src/lib/store.ts
@@ -3,11 +3,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './Features/counterSlice';
 import getRestaurant from './Features/getRestaurant';
+import user from './Features/userSlice';
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
-        restaurant: getRestaurant
+        restaurant: getRestaurant,
+        user: user,
     },
 });
 
