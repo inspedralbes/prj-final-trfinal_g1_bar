4d864986945b076cbf8fc19a5fe10675f435a66b
santipu03 <santipujolsau@gmail.com>
2024-02-07 08:49:09 +0100

    Connectar view logout amb store, API i storage

--- front/src/app/logout/page.tsx
+++ front/src/app/logout/page.tsx
@@ -1,17 +1,58 @@
 'use client'
 
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from "@/lib/Features/userSlice";
+import { RootState } from '@/lib/store';
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function Productes() {
-    
-    if(localStorage.getItem('user'))
-    console.log('logout', localStorage.getItem('user'));
-    localStorage.removeItem('user');
+    const router = useRouter();
+    const dispatch = useDispatch();
+    const userState = useSelector((state: RootState) => state.user);
+
+    let url = 'http://localhost:8000/api/logout';
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function logoutLaravel() {
+            try {
+                const response = await fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${userState.token}`,
+                        'Accept': 'application/json',
+                    }
+                });
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.log('Error al tancar la sessió');
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        logoutLaravel();                    // Tanca la sessió a Laravel
+        dispatch(logout());                 // Tanca la sessió a Redux
+        localStorage.removeItem('user');    // Tanca la sessió a localStorage
+        router.push('/');                   // Redirigeix a la pàgina principal
+    }, []);
 
     return (
-        <div className='pt-4 pb-5'>
-            <p>Logout...</p>
+        <div className='m-3'>
+            {loading ? (
+                <div className='d-flex justify-content-center'>
+                    <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                </div>
+            ) : (
+                <div>Sessió Tancada</div>
+            )}
         </div>
     );
 }
