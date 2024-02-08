30618d8e3d5530cb73e2808758cec11d799c6109
santipu03 <santipujolsau@gmail.com>
2024-02-08 10:01:23 +0100

    Adaptar header a la cistella

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -1,17 +1,21 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import { Offcanvas, Button } from 'react-bootstrap';
+import { Offcanvas, Button, Badge } from 'react-bootstrap';
 import { IconMenu2 } from '@tabler/icons-react';
+import { IconShoppingCart } from '@tabler/icons-react';
 import Link from 'next/link';
 import { RootState } from '@/lib/store';
 import { useSelector, useDispatch } from 'react-redux';
 import { login } from "@/lib/Features/userSlice";
 import Stack from 'react-bootstrap/Stack';
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
     const [show, setShow] = useState(false);
     const userToken = useSelector((state: RootState) => state.user.token);
+    const cistella = useSelector((state: RootState) => state.restaurant.tiquetIndividual);
     const dispatch = useDispatch();
+    const { push } = useRouter();
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -30,11 +34,13 @@ const Header = () => {
     }, [])
 
     return (
-        <header className="header p-3 flex justify-between items-center bg-zinc-300">
-            <Link href="/">
-                <img className="rounded" src="/paypart.png" alt="" width="50px" height="50px" />
-            </Link>
-            <h1>PayPart</h1>
+        <header className="header p-3 flex justify-between items-center bg-zinc-300 shadow-sm">
+            <div className='d-flex align-items-center gap-3'>
+                <Link href="/">
+                    <img className="rounded" src="/paypart.png" alt="" width="50px" height="50px" />
+                </Link>
+                <h1 className='m-0'>PayPart</h1>
+            </div>
             {!userToken ? (
                 <Button variant="primary" className='link-underline  link-underline-opacity-0'>
                     <Link href="/login" className='link-underline  link-underline-opacity-0 text-white'>
@@ -43,7 +49,13 @@ const Header = () => {
                 </Button>
             ) : (
                 <div className='d-flex'>
-                    <button onClick={handleShow}><IconMenu2 size={42} /></button>
+                    <div className='d-flex align-items-center gap-3'>
+                        <div className='position-relative'>
+                            <button onClick={() => push('/cistella')}><IconShoppingCart size={42} /></button>
+                            {cistella.length > 0 && <Badge bg="danger" pill style={{ position: 'absolute', top: '-3px', right: '-5px' }}>{cistella.length}</Badge>}
+                        </div>
+                        <button onClick={handleShow}><IconMenu2 size={42} /></button>
+                    </div>
                     <Offcanvas style={offcanvas} show={show} onHide={handleClose}>
                         <Offcanvas.Header closeButton>
                             <Offcanvas.Title>PayPart</Offcanvas.Title>
