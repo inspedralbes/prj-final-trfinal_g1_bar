fa0281c5718bca0faa2f8aa88437a9026ef78db3
santipu03 <santipujolsau@gmail.com>
2024-02-15 13:12:27 +0100

    Dades redux view productes

--- front/src/app/menu/productes/page.tsx
+++ front/src/app/menu/productes/page.tsx
@@ -4,64 +4,42 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { RootState } from "@/lib/store";
 import { useSelector, useDispatch } from 'react-redux';
-import { setProductesCategoriaVisualitzada, setProducteId } from "@/lib/Features/restaurantSlice";
-import GlobalConfig from '../../app.config'
+import { setProducteId } from "@/lib/Features/restaurantSlice";
 
 export default function Productes() {
-
-    /*PER ACCEDIR A LES DADES DE LA STORE */
-    const categoriaId = useSelector((state: RootState) => state.restaurant.categoriaId);
-
-    /*PER GUARDAR DADES A LA STORE */
     const dispatch = useDispatch();
 
+    const categoriaId = useSelector((state: RootState) => state.restaurant.categoriaId);
+    const productesPerCategoria = useSelector((state: RootState) => state.restaurant.productes);
+
     const [productes, setProductes] = useState<any>([]);
-    const [loading, setLoading] = useState(true);
-    const url = GlobalConfig.link + `/api/categories/${categoriaId}/productes`;
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(url);
-
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-
-                const data = await response.json();
-                setProductes(data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-    }, []); // <- s'ha d'especificar una array buida com a segon argument de useEffect per a que només s'executi un cop. Si no s'especifica res el hook useEffect s'executarà de manera infinita
-
 
     useEffect(() => {
-        dispatch(setProductesCategoriaVisualitzada(productes));
-    }, [productes]); // This useEffect logs and dispatches when productes changes
+        let productesArray = productesPerCategoria.filter((productes: any) => productes[0].categories[0].id === categoriaId);
+        productesArray = productesArray.flat();
+        console.log(productesArray);
+        setProductes(productesArray);
+    }, [productesPerCategoria]);
 
     return (
         <div className='pt-4 pb-5'>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <div className="d-flex flex-column align-items-center">
-                    {productes.map((producte : any) => (
-                        <Link key={producte.id} onClick={() => dispatch(setProducteId(producte.id))} href="/menu/productes/producte" className="link">
-                            <div className='item-categoria m-2 bg-zinc-300 rounded shadow d-flex flex-column justify-content-end align-items-center'>
-                                <img className="img-producte" src="/salad.png" alt="" />
-                                <div className='pb-3 text-center fw-bold text-uppercase'>
-                                    {producte.nom}
+            {productes.length === 0 ?
+                (
+                    <div className='text-center'>No hi ha productes en aquesta categoria</div>
+                ) : (
+                    <div className="d-flex flex-column align-items-center">
+                        {productes.map((producte: any, index: number) => (
+                            <Link key={index} onClick={() => dispatch(setProducteId(producte.id))} href="/menu/productes/producte" className="link">
+                                <div className='item-categoria m-2 bg-zinc-300 rounded shadow d-flex flex-column justify-content-end align-items-center'>
+                                    <img className="img-producte" src="/salad.png" alt="" />
+                                    <div className='pb-3 text-center fw-bold text-uppercase'>
+                                        {producte.nom}
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            )}
+                            </Link>
+                        ))}
+                    </div>
+                )}
         </div>
     );
 }
