9b53067a832afa0f64e86e1eb77974605a9a88b2
santipu03 <santipujolsau@gmail.com>
2024-02-16 08:17:45 +0100

    Implementar botó enrere al menú

--- front/src/app/menu/productes/page.tsx
+++ front/src/app/menu/productes/page.tsx
@@ -5,9 +5,13 @@ import Link from 'next/link';
 import { RootState } from "@/lib/store";
 import { useSelector, useDispatch } from 'react-redux';
 import { setProducteId } from "@/lib/Features/restaurantSlice";
+import { useRouter } from 'next/navigation';
+import { Button } from 'react-bootstrap';
+import { IconArrowLeft } from '@tabler/icons-react';
 
 export default function Productes() {
     const dispatch = useDispatch();
+    const { push } = useRouter();
 
     const categoriaId = useSelector((state: RootState) => state.restaurant.categoriaId);
     const productesPerCategoria = useSelector((state: RootState) => state.restaurant.productes);
@@ -22,24 +26,27 @@ export default function Productes() {
     }, [productesPerCategoria]);
 
     return (
-        <div className='pt-4 pb-5'>
-            {productes.length === 0 ?
-                (
-                    <div className='text-center'>No hi ha productes en aquesta categoria</div>
-                ) : (
-                    <div className="d-flex flex-column align-items-center">
-                        {productes.map((producte: any, index: number) => (
-                            <Link key={index} onClick={() => dispatch(setProducteId(producte.id))} href="/menu/productes/producte" className="link">
-                                <div className='item-categoria m-2 bg-zinc-300 rounded shadow d-flex flex-column justify-content-end align-items-center'>
-                                    <img className="img-producte" src="/salad.png" alt="" />
-                                    <div className='pb-3 text-center fw-bold text-uppercase'>
-                                        {producte.nom}
-                                    </div>
-                                </div>
-                            </Link>
-                        ))}
+        <div className='py-3'>
+            <Button onClick={() => push('/menu')} className="mb-2 ml-3 shadow">
+                <div className='flex align-items-center'>
+                    <IconArrowLeft size={24} />
+                    <div className='mr-2 ml-1'>
+                        Enrere
                     </div>
-                )}
+                </div>
+            </Button>
+            <div className="d-flex flex-column align-items-center">
+                {productes.map((producte: any, index: number) => (
+                    <Link key={index} onClick={() => dispatch(setProducteId(producte.id))} href="/menu/productes/producte" className="link">
+                        <div className='item-categoria m-2 bg-zinc-300 rounded shadow d-flex flex-column justify-content-end align-items-center'>
+                            <img className="img-producte" src="/salad.png" alt="" />
+                            <div className='pb-3 text-center fw-bold text-uppercase'>
+                                {producte.nom}
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 }
