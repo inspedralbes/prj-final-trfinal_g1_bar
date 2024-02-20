9b9570bd545b30cce6f498f4c79966778c87a0ad
= <=>
2024-02-20 11:04:08 +0100

    agvas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,13 @@
 describe('template spec', () => {
   it('login', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000/?restaurantId=1&tableId=1')
     cy.contains('Login').click()
     cy.url().should('include', '/login')
     cy.get('input[type="email"]').type('miquel@gmail.com');
     cy.get('input[type="password"]').type('miquel');
     cy.get('input[type="submit"]').click();
+    cy.contains('MENÚ').click();
+    cy.url().should('include', '/menu')
   })
 
   it('buy', () => {
@@ -24,6 +26,7 @@ describe('template spec', () => {
     cy.get('.tabler-icon-menu-2').click();
     cy.contains('Comanda').click();
     cy.contains('Pagar').click();
+    cy.url().should('include', '/pagament')
   })
 
   it('logout', () => {
@@ -36,5 +39,6 @@ describe('template spec', () => {
     cy.get('.tabler-icon-menu-2').click();
     cy.contains('Tancar Sessió').click();
     cy.contains('Tancar Sessió').click();
+    cy.url().should('eq', 'http://localhost:3000/');
   })
 })
